732573fc9b99203ee6077505d9733a1bb1788d24
Mạnh Dương <46205571+luxysiv@users.noreply.github.com>
2024-05-15 15:30:37 +0700

    Update extract_version.pl

--- utils/extract_version.pl
+++ utils/extract_version.pl
@@ -2,7 +2,12 @@
 use strict;
 use warnings;
 
-my $file = shift or die "Usage: $0 filename\n";
+my $pattern = shift or die "Usage: $0 'filename_pattern'\n";
+
+my @files = glob($pattern);
+die "No files matched the pattern '$pattern'\n" unless @files;
+
+my $file = $files[0];
 
 while ($file =~ /(\d+(\.\d+)+)/g) {
     print "$1\n";
