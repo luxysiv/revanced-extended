305b54ba9df6d6d2fe689595def392fdaea2c74e
Mạnh Dương <46205571+luxysiv@users.noreply.github.com>
2024-05-26 00:54:43 +0700

    Small change

--- utils/github_downloader.pm
+++ utils/github_downloader.pm
@@ -9,30 +9,36 @@ use LWP::UserAgent;
 use HTTP::Request;
 use POSIX qw(strftime);
 
+# Export the download_resources function
 our @EXPORT_OK = qw(download_resources);
 
+# Subroutine to perform an HTTP GET request and handle the response
 sub req {
     my ($url, $output) = @_;
     $output ||= '-';
 
+    # Create a user agent object
     my $ua = LWP::UserAgent->new(
         agent => 'Mozilla/5.0 (Android 13; Mobile; rv:125.0) Gecko/125.0 Firefox/125.0',
         timeout => 30,
     );
 
+    # Create an HTTP GET request
     my $request = HTTP::Request->new(GET => $url);
     my $response = $ua->request($request);
 
+    # Get the current timestamp
     my $timestamp = strftime("%Y-%m-%d %H:%M:%S", localtime);
     if ($response->is_success) {
         my $size = length($response->decoded_content);
+        my $final_url = $response->base;
         if ($output ne '-') {
             open(my $fh, '>', $output) or die "Could not open file '$output' $!";
             print $fh $response->decoded_content;
             close($fh);
-            print "$timestamp URL:$url [$size] -> \"$output\" \n";
+            print "$timestamp URL:$final_url [$size/$size] -> \"$output\" [1]\n";
         } else {
-            print "$timestamp URL:$url [$size/] -> \"-\" \n";
+            print "$timestamp URL:$final_url [$size/$size] -> \"-\" [1]\n";
         }
         return $response->decoded_content;
     } else {
@@ -40,6 +46,7 @@ sub req {
     }
 }
 
+# Subroutine to download resources from specified GitHub repositories
 sub download_resources {
     my @repos = qw(revanced-patches revanced-cli revanced-integrations);
 
