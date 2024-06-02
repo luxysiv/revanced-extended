4db712121ddc608658e2d735cfbf69e7d2d82792
Mạnh Dương <46205571+luxysiv@users.noreply.github.com>
2024-06-02 18:08:08 +0700

    Switch APK source

--- revanced.pl
+++ revanced.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
-use FindBin; 
+use FindBin;
 use Env;
 
 use lib "$FindBin::Bin/utils";
@@ -12,31 +12,84 @@ use apply_patches qw(apply_patches);
 use github_release qw(github_release);
 use github_downloader qw(download_resources);
 
-
 # Main
 # Could set specific version to patch. Example:
 # $ENV{VERSION} = "6.51.52";
 
-# Download Github releases assets 
+# Download Github releases assets
 download_resources();
 
 # Patch YouTube
-apkmirror(
-    "google-inc", 
-    "youtube", 
-    "com.google.android.youtube"
-);
-apply_patches("youtube");
-github_release("youtube");
+my $success = eval {
+    apkmirror(
+        "google-inc",
+        "youtube",
+        "com.google.android.youtube"
+    );
+    apply_patches("youtube");
+    github_release("youtube");
+    1;
+};
 
-# Patch YouTube Music 
-apkmirror(
-    "google-inc", 
-    "youtube-music", 
-    "com.google.android.apps.youtube.music", 
-    "arm64-v8a"
-);
-apply_patches("youtube-music");
-github_release("youtube-music");
+unless ($success) {
+    $success = eval {
+        uptodown(
+            "youtube",
+            "com.google.android.youtube"
+        );
+        apply_patches("youtube");
+        github_release("youtube");
+        1;
+    };
+
+    unless ($success) {
+        eval {
+            apkpure(
+                "youtube",
+                "com.google.android.youtube"
+            );
+            apply_patches("youtube");
+            github_release("youtube");
+            1;
+        };
+    }
+}
+
+# Patch YouTube Music
+$success = eval {
+    apkmirror(
+        "google-inc",
+        "youtube-music",
+        "com.google.android.apps.youtube.music",
+        "arm64-v8a"
+    );
+    apply_patches("youtube-music");
+    github_release("youtube-music");
+    1;
+};
+
+unless ($success) {
+    $success = eval {
+        uptodown(
+            "youtube-music",
+            "com.google.android.apps.youtube.music"
+        );
+        apply_patches("youtube-music");
+        github_release("youtube-music");
+        1;
+    };
+
+    unless ($success) {
+        eval {
+            apkpure(
+                "youtube-music",
+                "com.google.android.apps.youtube.music"
+            );
+            apply_patches("youtube-music");
+            github_release("youtube-music");
+            1;
+        };
+    }
+}
 
 # You can add other apps here
