546ca43b326ace038c6c7f27bec41192d0f345d8
Mạnh Dương <46205571+luxysiv@users.noreply.github.com>
2024-06-03 08:18:54 +0700

    Use matrix for faster

--- revanced.pl
+++ revanced.pl
@@ -3,6 +3,9 @@ use strict;
 use warnings;
 use FindBin;
 use Env;
+use JSON;
+use File::Spec;
+use IO::File;
 
 use lib "$FindBin::Bin/utils";
 use apkpure qw(apkpure);
@@ -12,84 +15,69 @@ use apply_patches qw(apply_patches);
 use github_release qw(github_release);
 use github_downloader qw(download_resources);
 
-# Main
-# Could set specific version to patch. Example:
-# $ENV{VERSION} = "6.51.52";
+sub download_and_patch_app {
+    my ($app_info) = @_;
 
-# Download Github releases assets
-download_resources();
-
-# Patch YouTube
-my $success = eval {
-    apkmirror(
-        "google-inc",
-        "youtube",
-        "com.google.android.youtube"
-    );
-    apply_patches("youtube");
-    github_release("youtube");
-    1;
-};
-
-unless ($success) {
-    $success = eval {
-        uptodown(
-            "youtube",
-            "com.google.android.youtube"
+    my $success = eval {
+        apkmirror(
+            $app_info->{apkmirror}->{org},
+            $app_info->{apkmirror}->{name},
+            $app_info->{package},
+            $app_info->{apkmirror}->{arch},
+            $app_info->{apkmirror}->{dpi}
         );
-        apply_patches("youtube");
-        github_release("youtube");
+        apply_patches($app_info->{apkmirror}->{name});
+        github_release($app_info->{apkmirror}->{name});
         1;
     };
 
     unless ($success) {
-        eval {
-            apkpure(
-                "youtube",
-                "com.google.android.youtube"
+        $success = eval {
+            uptodown(
+                $app_info->{uptodown}->{name},
+                $app_info->{package}
             );
-            apply_patches("youtube");
-            github_release("youtube");
+            apply_patches($app_info->{uptodown}->{name});
+            github_release($app_info->{uptodown}->{name});
             1;
         };
     }
-}
-
-# Patch YouTube Music
-$success = eval {
-    apkmirror(
-        "google-inc",
-        "youtube-music",
-        "com.google.android.apps.youtube.music",
-        "arm64-v8a"
-    );
-    apply_patches("youtube-music");
-    github_release("youtube-music");
-    1;
-};
-
-unless ($success) {
-    $success = eval {
-        uptodown(
-            "youtube-music",
-            "com.google.android.apps.youtube.music"
-        );
-        apply_patches("youtube-music");
-        github_release("youtube-music");
-        1;
-    };
 
     unless ($success) {
-        eval {
+        $success = eval {
             apkpure(
-                "youtube-music",
-                "com.google.android.apps.youtube.music"
+                $app_info->{apkpure}->{name},
+                $app_info->{package}
             );
-            apply_patches("youtube-music");
-            github_release("youtube-music");
+            apply_patches($app_info->{apkpure}->{name});
+            github_release($app_info->{apkpure}->{name});
             1;
         };
     }
+
+    return $success;
 }
 
-# You can add other apps here
+sub read_json_file {
+    my ($file_path) = @_;
+    my $file = IO::File->new($file_path, "r") or die "Could not open file '$file_path': $!";
+    my $json_text = do { local $/; <$file> };
+    $file->close;
+    my $json = decode_json($json_text);
+    return $json;
+}
+
+sub main {
+    my $app_name = shift @ARGV or die "App name not provided";
+
+    download_resources();
+
+    my $json_file = File::Spec->catfile($FindBin::Bin, 'apps', "$app_name.json");
+
+    my $app_info = read_json_file($json_file);
+    my $success = download_and_patch_app($app_info);
+    unless ($success) {
+    }
+}
+
+main();
