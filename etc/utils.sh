204f94ac2c156e089aa5ffbacd3c8ae975c616fd
Mạnh Dương <46205571+luxysiv@users.noreply.github.com>
2024-12-12 07:55:38 +0700

    Fix patches name

--- etc/utils.sh
+++ etc/utils.sh
@@ -157,8 +157,8 @@ apply_patches() {
         for line in "${lines[@]}"; do
             if [[ -n "$line" && ( ${line:0:1} == "+" || ${line:0:1} == "-" ) ]]; then
                 patch_name=$(sed -e 's/^[+|-] *//;s/ *$//' <<< "$line") 
-                [[ ${line:0:1} == "+" ]] && includePatches+=("--include" "$patch_name")
-                [[ ${line:0:1} == "-" ]] && excludePatches+=("--exclude" "$patch_name")
+                [[ ${line:0:1} == "+" ]] && includePatches+=("-e" "$patch_name")
+                [[ ${line:0:1} == "-" ]] && excludePatches+=("-d" "$patch_name")
             fi
         done
     fi
